a49dcd78d9ab4ea180b85cca2d6af8f9d6984de0
Александр Щербаков <al.scherbakov.263@gmail.com>
2022-09-18 20:26:43 +0300

    lab6 complete

--- src/main/java/server/ServerReceiver.java
+++ src/main/java/server/ServerReceiver.java
@@ -1,25 +1,16 @@
 package server;
 
-import command.Command;
-import command.CommandEnum;
 import command.Receiver;
-import messages.AddElementMessage;
 import messages.Message;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.DatagramChannel;
-import java.util.stream.Stream;
 
 public class ServerReceiver extends Thread{
     private DatagramSocket socket;
-    private DatagramChannel channel;
 
     private Server server;
     private Object clientMessage;
@@ -29,13 +20,6 @@ public class ServerReceiver extends Thread{
         this.socket = datagramSocket;
         this.state = state;
         this.server = server;
-    }
-
-    private static String extractMessage(ByteBuffer buffer) {
-        buffer.flip();
-        byte[] bytes = new byte[buffer.remaining()];
-        buffer.get(bytes);
-        return new String(bytes);
     }
 
     @Override
